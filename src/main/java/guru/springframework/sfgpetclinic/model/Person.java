4c0b6d0321f0f69665318a05853044f2abd9744f
Andrzej Szypulski <szypul86@gmail.com>
2019-11-25 13:28:30 +0100

    POJO data model

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+    private String firstName;
+    private String lastName;
+
+
+
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PetType {
+
+    private String name;
+
+}
